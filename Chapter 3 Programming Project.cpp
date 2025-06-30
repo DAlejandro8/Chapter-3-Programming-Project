acabb6aa786917cce07661dcd0a9f5a7bb46df60
legod <legod@BRICK>
2025-06-29 23:38:36 -0400

    done

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -11,10 +11,10 @@ int main()
     std::cout << "How many cookies would you like to make?";
     std::cin >> Ncookies;
 
-    double ratio = Ncookies / 48;
+    double ratio = Ncookies / 48;//the recipe is for 48 cookies
 
     double sugar = 1.5 * ratio;
-    double flour = ratio;
+    double flour = ratio;//it's a one to one ratio so I just made it equal
     double butter = 2.75 * ratio;
 
    std:: cout << "To bake " << Ncookies << " cookies, you need " << sugar << " cups of sugar, " << flour << " cups of flour, and " << butter << " cups of buter. \n";
@@ -36,16 +36,6 @@ int main()
     
     
     return 0;
-    std::cout << "Hello World!\n";
+    
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
