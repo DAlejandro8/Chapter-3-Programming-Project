f0e322f69282ad32f00becb64561836cb3d55359
legod <legod@BRICK>
2025-06-29 21:33:10 -0400

    not finished

--- Chapter 3 Programming Project.cpp
+++ Chapter 3 Programming Project.cpp
@@ -2,10 +2,38 @@
 //
 
 #include <iostream>
+#include <random>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    std::random_device first;
+    std::uniform_real_distribution<int> randomReal(0.0, 500);
+
+
+
+
+
+
+
+    double Ncookies;
+
+    std::cout << "How many cookies would you like to make?";
+    std::cin >> Ncookies;
+
+    double ratio = Ncookies / 48;
+
+    double sugar = 1.5 * ratio;
+    double flour = ratio;
+    double butter = 2.75 * ratio;
+
+    std::cout << "To bake " << Ncookies << " cookies, you need " << sugar << " cups of sugar, " << flour << " cups of flour, and " << butter << " cups of buter. \n";
+
+
+
+
+
+
 }
 
 
